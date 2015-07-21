25992a4170db87c92b95ee8c723e1f2eb2c1b949
markpeace <mark@learnsomestuff.com>
2015-07-21 18:24:17 +0000

    better bottom drawe

--- pu.sh
+++ pu.sh
@@ -8,7 +8,7 @@ git commit -a -m "$*"
 git clean -f
 git push origin master
 
-curl -u mark@learnsomestuff.com -X PUT -d 'data={"pull":"true"}' https://build.phonegap.com/api/v1/apps/1555445
+curl -u mark@learnsomestuff.com -X PUT -d 'data={"pull":"true"}' https://build.phonegap.com/api/v1/apps/1558732
 
 ionic upload
 
